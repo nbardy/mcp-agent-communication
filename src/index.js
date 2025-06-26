04fb436199f227064a582ec0c9e4429bc4e0e42a
Nicholas Bardy <nicholasbardy@gmail.com>
2025-06-25 19:02:10 -0700

    Clean up build configuration and output to dist folder

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { startServer } from './server';
-export { MCPClient } from './client';
-export * from './types';
-export { handlePut, handleList, handleRead, handleGather, dispatch } from './bank';
